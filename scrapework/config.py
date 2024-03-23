4e514ac2963b1d72471b6171723ed371d9e0d605
Stéphane Busso <stephane.busso@gmail.com>
2024-03-23 21:30:11 +1300

    deactivate env vars

--- scrapework/config.py
+++ scrapework/config.py
@@ -1,18 +1,18 @@
 import enum
-import os
 
 from dotenv import load_dotenv
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 load_dotenv()
 
 
 class Config(BaseModel):
-    S3_ENDPOINT: str = Field(default=os.environ["AWS_ENDPOINT_URL"])
-    S3_BUCKET: str = Field(default=os.environ["S3_BUCKET"])
-    S3_ACCESS_KEY_ID: str = Field(default=os.environ["AWS_ACCESS_KEY_ID"])
-    S3_SECRET_ACCESS_KEY: str = Field(default=os.environ["AWS_SECRET_ACCESS_KEY"])
-    TELEGRAM_SENDER_TOKEN: str = Field(default=os.environ["TELEGRAM_SENDER_TOKEN"])
+    # S3_ENDPOINT: str = Field(default=os.environ["AWS_ENDPOINT_URL"])
+    # S3_BUCKET: str = Field(default=os.environ["S3_BUCKET"])
+    # S3_ACCESS_KEY_ID: str = Field(default=os.environ["AWS_ACCESS_KEY_ID"])
+    # S3_SECRET_ACCESS_KEY: str = Field(default=os.environ["AWS_SECRET_ACCESS_KEY"])
+    # TELEGRAM_SENDER_TOKEN: str = Field(default=os.environ["TELEGRAM_SENDER_TOKEN"])
+    pass
 
 
 config = Config()
